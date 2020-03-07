d4871624823f6c786a18e89dfe346ba1feb95477
tushar <gupta.tushar68@gmail.com>
2020-03-07 20:12:01 +0530

    Splash Screen and Login module added/initiated.

---
+++ app/src/main/java/com/example/nitctraveltogether/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.nitctraveltogether;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#ff000000"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void register(View view)
+    {
+        final AutoCompleteTextView email,password;
+
+
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
+
+
+        final String remail=email.getText().toString();
+        final String rpassword=password.getText().toString();
+        mAuth.createUserWithEmailAndPassword(remail, rpassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(MainActivity.this, "Email Registered, Please check your email for verification",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }else{
+                                        Toast.makeText(MainActivity.this, task.getException().getMessage(),
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+    public void login(View view)
+    {
+        AutoCompleteTextView emailv,passwordv;
+        emailv= findViewById(R.id.email);
+        passwordv=findViewById(R.id.password);
+
+        String email=emailv.getText().toString();
+        String password=passwordv.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if(mAuth.getCurrentUser().isEmailVerified()){
+                                Toast.makeText(MainActivity.this, "Authentication Success.",
+                                        Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "Please verify your email.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+}
