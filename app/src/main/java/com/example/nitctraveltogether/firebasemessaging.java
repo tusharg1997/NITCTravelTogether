ddaeb7d9cc3751e0867265123be861ea4c5cf7e0
tushar <gupta.tushar68@gmail.com>
2020-03-18 22:53:31 +0530

    Push notification added using firebase, send request will send a notification to offer lift user

---
+++ app/src/main/java/com/example/nitctraveltogether/firebasemessaging.java
@@ -0,0 +1,20 @@
+package com.example.nitctraveltogether;
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+public class firebasemessaging extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage.getNotification()!=null)
+        {
+            String title=remoteMessage.getNotification().getTitle();
+            String text=remoteMessage.getNotification().getBody();
+
+            //notificationhelper.displaynotification(getApplicationContext(),title,text);
+        }
+    }
+}
